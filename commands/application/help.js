2bc605dc4f9afb7de8f2e56ef1153ed6eb3a52df
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2022-04-22 19:02:11 +0200

    Update help.js

--- commands/application/help.js
+++ commands/application/help.js
@@ -18,7 +18,7 @@ module.exports = {
             { name: '\u200B', value: '\u200B' },
             { name: 'Demo', value: 'The following is a visual representation of how the bot works:', inline: false },
           )
-          .setImage('https://media.giphy.com/media/LrqUuAZB2E3hGvHtOH/giphy.gif')
+          .setImage('https://media.giphy.com/media/9jkdG3K7RAV0Ri9npP/giphy.gif')
           .setFooter({ text: 'Got feedback? Join the 98FE server: https://discord.gg/rbRX3Dv5TG', iconURL: 'https://cdn.iconscout.com/icon/free/png-128/discord-3-569463.png' });
         return interaction.reply({embeds: [embed]});
   },
