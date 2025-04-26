cd116977d98eeeb5dfcd3ac0b92826f3966a1d57
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2025-04-26 18:18:34 +0200

    Update cargo.js

--- commands/application/cargo.js
+++ commands/application/cargo.js
@@ -19,22 +19,29 @@ module.exports = {
   			.setRequired(true)),
   async execute(interaction) {
     await interaction.deferReply();
-    const character = this.getCharacter(interaction.options.getString('character'));
-    const mov = interaction.options.getString('move');
-    if (mov.split("?")[1] === undefined) {
-      return interaction.editReply('You are free to manually enter the **character** but you have to select the **move** from the scroll list. You can type to refine the search.')
-    }
-    const [id, move] = mov.split("?");
-    console.log("cargo", character, move)
-
     try {
+      const character = this.getCharacter(interaction.options.getString('character'));
+      const id = interaction.options.getString('move');
+      /*if (mov.split("?")[1] === undefined) {
+        return interaction.editReply('You are free to manually enter the **character** but you have to select the **move** from the scroll list. You can type to refine the search.')
+      }
+      const [id, move] = mov.split("?");*/
+      
       // Fetch the cargo data with the appropriate moveId
-      const url_cargo = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_KOF98FE%2C&&fields=MoveData_KOF98FE.rank%2C+MoveData_KOF98FE.idle%2C+MoveData_KOF98FE.images%2C+MoveData_KOF98FE.hitboxes%2C+MoveData_KOF98FE.damage%2C+MoveData_KOF98FE.counter%2C+MoveData_KOF98FE.stun%2C+MoveData_KOF98FE.guard%2C+MoveData_KOF98FE.cancel%2C+MoveData_KOF98FE.startup%2C+MoveData_KOF98FE.active%2C+MoveData_KOF98FE.recovery%2C+MoveData_KOF98FE.hitadv%2C+MoveData_KOF98FE.blockadv%2C+MoveData_KOF98FE.invul%2C&where=moveId%3D%22"+encodeURIComponent(id)+"%22&order+by=&limit=100&format=json";
+      const url_cargo = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_KOF98FE%2C&&fields=MoveData_KOF98FE.input%2C+MoveData_KOF98FE.input2%2C+MoveData_KOF98FE.name%2C+MoveData_KOF98FE.rank%2C+MoveData_KOF98FE.idle%2C+MoveData_KOF98FE.images%2C+MoveData_KOF98FE.hitboxes%2C+MoveData_KOF98FE.damage%2C+MoveData_KOF98FE.counter%2C+MoveData_KOF98FE.stun%2C+MoveData_KOF98FE.guard%2C+MoveData_KOF98FE.cancel%2C+MoveData_KOF98FE.startup%2C+MoveData_KOF98FE.active%2C+MoveData_KOF98FE.recovery%2C+MoveData_KOF98FE.hitadv%2C+MoveData_KOF98FE.blockadv%2C+MoveData_KOF98FE.invul%2C&where=moveId%3D%22"+encodeURIComponent(id)+"%22&order+by=&limit=100&format=json";
       const response_cargo = await fetch(url_cargo);
       const cargo = await response_cargo.json();
   
       // Preparing the embed data from cargo
       let moveData = cargo[0];
+      let move = moveData["name"]
+			if (moveData["input"] !== null) {
+        move = moveData["name"] + " (" + moveData["input"] + ")"
+        if (moveData["input2"] !== null && moveData["input"] !== moveData["input2"]) {
+          move = moveData["name"] + " ([" + moveData["input"] + "] / [" + moveData["input2"] + "])"
+        }
+      }
+      console.log("cargo", character, move)
       const startup = this.getHyperLink(moveData['startup']);
       const active = this.getHyperLink(moveData['active']);
       const recovery = this.getHyperLink(moveData['recovery']);
